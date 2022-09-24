e4b6565b6c0f336a5dd5236ee7c7b1cdc49da3ef
Anton <104672550+Samusev-Anton@users.noreply.github.com>
2022-09-24 17:18:04 +0300

    changes

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import markupGallery from '../templates/gallery.hbs'
+
+import {galleryItems} from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+console.log(markupGallery);
+const imageConteiner = document.querySelector('.gallery');
+const imageMarkup = createGallery(galleryItems);
+imageConteiner.insertAdjacentHTML('beforeend', imageMarkup);
+
+function createGallery(galleryItems) {
+    return galleryItems.map(markupGallery).join('');
+}
+// console.log(markup);
+imageConteiner.addEventListener('click', onClickImage);
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250
+});
+function onClickImage(evt) {
+    evt.preventDefault()
+    if (evt.target.nodeName !== "IMG") {
+        return;
+    }
+  }
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,45 @@
+import throttle from "lodash.throttle";
+const form = document.querySelector('.feedback-form');
+const textarea = document.querySelector('textarea');
+const inputArea =document.querySelector('input')
+const formData = {};
+
+populateFormData();
+
+
+form.addEventListener('submit', onFormSubmit);
+// textarea.addEventListener('input', throttle(onTextareaInput, 1000));
+form.addEventListener('input', throttle(onFormInput, 1000));
+
+function onFormInput(evt) {
+    formData[evt.target.name] = evt.target.value;
+    localStorage.setItem('formData', JSON.stringify(formData));
+}
+
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    const lenghtFormData = Object.keys(formData).length;
+
+    if (lenghtFormData < 2) {
+        alert('поля не должны быть пустыми');
+    }
+    console.log(formData);
+    evt.currentTarget.reset();
+    localStorage.removeItem('formData');
+    for(var key in formData){
+        delete formData[key];
+}
+}
+
+function populateFormData() {
+    const savedMessage = localStorage.getItem('formData');
+    const formSavedMessage = JSON.parse(savedMessage);
+    console.log(formSavedMessage);
+    if (savedMessage) {
+        console.log(savedMessage);
+        textarea.value = formSavedMessage.message;
+        inputArea.value = formSavedMessage.email;
+    }
+}
 
